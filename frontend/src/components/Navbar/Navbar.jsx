2016311d44409c295f06d6e3feae0eda43ef6fbb
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-11 21:00:10 +0300

    hover the order and logout on the image

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = ({ setShowLogin }) => {
         <button onClick={()=>setShowLogin(true)}>sign in</button> :
         <div className='navbar-profile'>
           <img src={assets.profile_icon} alt="" />
-          <ul>
+          <ul className='nav-profile-dropdown'>
             <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
             <hr />
             <li><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
