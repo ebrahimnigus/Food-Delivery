695b8e52b77ad947db903aebcc6a56ab334fd916
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-12 16:45:45 +0300

    create api for cart

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { addToCart,removeFromCart,getCart } from '../controllers/cartController.js'
+import authMiddleware from '../middleware/auth.js';
+
+const cartRouter = express.Router();
+
+cartRouter.post('/add',addToCart)
+cartRouter.post('/remove',removeFromCart)
+cartRouter.post('/get',getCart)
+
+export default cartRouter;
