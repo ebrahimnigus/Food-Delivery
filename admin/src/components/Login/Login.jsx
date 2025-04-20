6225241d00e219d281b5c2d6d9f472b24f618caa
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-20 17:31:30 +0300

    update Login

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -13,6 +13,7 @@ const Login = ({ url }) => {
     email: "",
     password: "",
   });
+  
   const onChangeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
