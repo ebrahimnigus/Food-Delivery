84994c206b0dde226579e06b9023abf4edb348eb
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-21 21:37:36 +0300

    Add authentication in admin pannel

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import  './Sidebar.css'
+import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
@@ -8,16 +8,16 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="sidebar-options">
         <NavLink to='/add' className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Add Items</p>
+            <img src={assets.add_icon} alt="" />
+            <p>Add Items</p>
         </NavLink>
-        <NavLink  to='/list' className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>List Items</p>
+        <NavLink to='/list' className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>List Items</p>
         </NavLink>
-        <NavLink to="/orders" className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>Orders</p>
+        <NavLink to='/orders' className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>Orders</p>
         </NavLink>
       </div>
     </div>
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,88 +1,91 @@
 import React, { useState } from 'react'
 import './Add.css'
-import { assets } from '../../assets/assets'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import { assets,url } from '../../assets/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const Add = ({url}) => {
+const Add = () => {
 
-    const [image,setImage] = useState(false);
     const [data, setData] = useState({
         name: "",
         description: "",
         price: "",
         category: "Salad"
-    })
+    });
 
-    const onChangeHandler = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setData(data=>({...data,[name]:value}))
-    }
+    const [image, setImage] = useState(false);
+
     const onSubmitHandler = async (event) => {
         event.preventDefault();
         const formData = new FormData();
-        formData.append("name", data.name)
-        formData.append("description", data.description)
-        formData.append("price", Number(data.price))
-        formData.append("category", data.category)
-        formData.append("image", image)
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("price", Number(data.price));
+        formData.append("category", data.category);
+        formData.append("image", image);
         const response = await axios.post(`${url}/api/food/add`, formData);
         if (response.data.success) {
+            toast.success(response.data.message)
             setData({
                 name: "",
                 description: "",
                 price: "",
                 category: "Salad"
             })
-            setImage(false)
-            toast.success(response.data.message)
+            setImage(false);
         }
         else{
-            toast.error(response.data.message);
+            toast.error(response.data.message)
         }
     }
-  return (
-    <div className='add'>
-        <form className='flex-col' onSubmit={onSubmitHandler}>
-            <div className='add-img-upload flex-col'>
-                <p>Upload Image</p>
-                <label htmlFor="image">
-                    <img src={image? URL.createObjectURL(image):assets.upload_area} alt="" />
-                </label>
-                <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required/>
-            </div>
-            <div className="app-product-name flex-col">
-                <p>Product name</p>
-                <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here'/>
-            </div>
-            <div className='add-product-description flex-col'>
-                <p>Product description</p>
-                <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='Write content here' required></textarea>
-            </div>
-            <div className="add-category-price">
-                <div className="add-category flex-col">
-                    <p>Product category</p>
-                    <select onChange={onChangeHandler} name="category" >
-                        <option value="Salad">Salad</option>
-                        <option value="Rolls">Rolls</option>
-                        <option value="Deserts">Deserts</option>
-                        <option value="Sandwich">Sandwich</option>
-                        <option value="Cake">Cake</option>
-                        <option value="Pure Veg">Pure Veg</option>
-                        <option value="Pasta">Pasta</option>
-                        <option value="Noodles">Noodles</option>
-                    </select>
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    return (
+        <div className='add'>
+            <form className='flex-col' onSubmit={onSubmitHandler}>
+                <div className='add-img-upload flex-col'>
+                    <p>Upload image</p>
+                    <label htmlFor="image">
+                        <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" />
+                    </label>
+                    <input onChange={(e) => { setImage(e.target.files[0]) }} type="file" id="image" hidden required />
                 </div>
-                <div className="add-price flex-col">
-                    <p>Product price</p>
-                    <input onChange={onChangeHandler} value={data.price} type="number" name='price' placeholder='$20'/>
+                <div className='add-product-name flex-col'>
+                    <p>Product name</p>
+                    <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Type here' required />
                 </div>
-            </div>
-            <button type='submit' className='add-btn'>ADD</button>
-        </form>
-    </div>
-  )
+                <div className='add-product-description flex-col'>
+                    <p>Product description</p>
+                    <textarea name='description' onChange={onChangeHandler} value={data.description} type="text" rows={6} placeholder='Write content here' required />
+                </div>
+                <div className='add-category-price'>
+                    <div className='add-category flex-col'>
+                        <p>Product category</p>
+                        <select name='category' onChange={onChangeHandler} >
+                            <option value="Salad">Salad</option>
+                            <option value="Rolls">Rolls</option>
+                            <option value="Deserts">Deserts</option>
+                            <option value="Sandwich">Sandwich</option>
+                            <option value="Cake">Cake</option>
+                            <option value="Pure Veg">Pure Veg</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Noodles">Noodles</option>
+                        </select>
+                    </div>
+                    <div className='add-price flex-col'>
+                        <p>Product Price</p>
+                        <input type="Number" name='price' onChange={onChangeHandler} value={data.price} placeholder='$25' />
+                    </div>
+                </div>
+                <button type='submit' className='add-btn' >ADD</button>
+            </form>
+        </div>
+    )
 }
 
 export default Add
