98faec2a3986ec976d9c79ba9010deff1b53db49
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-22 11:50:13 +0300

    update frontend url

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,52 +1,54 @@
 import foodModel from "../models/foodModel.js";
-import fs from "fs";
-
-// Add food item
-
-const addFood = async (req, res) => {
-    let image_filename = `${req.file.filename}`;
-
-    const food = new foodModel ({
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        category:req.body.category,
-        image: image_filename
-    })
-    try {
-        await food.save();
-        res.json({success:true, message:"Food Added"})
-    } catch (error) {
-        console.log(error)
-        res.json({success:false, message:"Error"})
-    }
-
-}
+import fs from 'fs'
 
 // all food list
 const listFood = async (req, res) => {
     try {
-        const foods = await foodModel.find({});
-        res.json({success:true, data:foods})
+        const foods = await foodModel.find({})
+        res.json({ success: true, data: foods })
     } catch (error) {
         console.log(error);
-        res.json({success: false, message: "Error"})
+        res.json({ success: false, message: "Error" })
     }
+
 }
 
-// remove food item
+// add food
+const addFood = async (req, res) => {
 
-const removeFood = async (req,res) => {
+    let image_filename = `${req.file.filename}`
+
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category:req.body.category,
+        image: image_filename,
+    })
     try {
-        const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, ()=>{})
-
-        await foodModel.findByIdAndDelete(req.body.id);
-        res.json({success:true, message:"Food Removed"})
+        await food.save();
+        res.json({ success: true, message: "Food Added" })
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" })
     }
 }
 
-export { addFood, listFood, removeFood};
+// delete food
+const removeFood = async (req, res) => {
+    try {
+
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => { })
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Food Removed" })
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+
+}
+
+export { listFood, addFood, removeFood }
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-// placing user order for frontend
+const currency = "usd";
+const deliveryCharge = 2;
+const frontend_URL = 'http://localhost:4000';
+
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-delivery-frontend-s2l9.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -19,7 +20,7 @@ const placeOrder = async (req, res) => {
 
     const line_items = req.body.items.map((item) => ({
       price_data: {
-        currency: "usd",
+        currency,
         product_data: {
           name: item.name,
         },
@@ -30,20 +31,20 @@ const placeOrder = async (req, res) => {
 
     line_items.push({
       price_data: {
-        currency: "usd",
+        currency,
         product_data: {
-          name: "Delivery Charges",
+          name: "Delivery Charge",
         },
-        unit_amount: 2 * 100,
+        unit_amount: deliveryCharge * 100,
       },
       quantity: 1,
     });
 
     const session = await stripe.checkout.sessions.create({
+      success_url: `http://localhost:5173/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `http://localhost:5173/verify?success=false&orderId=${newOrder._id}`,
       line_items: line_items,
-      mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      mode: 'payment',
     });
 
     res.json({ success: true, session_url: session.url });
@@ -53,23 +54,16 @@ const placeOrder = async (req, res) => {
   }
 };
 
-const verifyOrder = async (req, res) => {
-  const { orderId, success } = req.body;
+const listOrders = async (req, res) => {
   try {
-    if (success == "true") {
-      await orderModel.findByIdAndUpdate(orderId, { payment: true });
-      res.json({ success: true, message: "Paid" });
-    } else {
-      await orderModel.findByIdAndDelete(orderId);
-      res.json({ success: false, message: "Not Paid" });
-    }
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
   }
 };
 
-// user orders for frontend
 const userOrders = async (req, res) => {
   try {
     const orders = await orderModel.find({ userId: req.body.userId });
@@ -80,38 +74,28 @@ const userOrders = async (req, res) => {
   }
 };
 
-// Listing orders for admin pannel
-const listOrders = async (req, res) => {
-  try {
-    let userData = await userModel.findById(req.body.userId);
-    if (userData && userData.role === "admin") {
-      const orders = await orderModel.find({});
-      res.json({ success: true, data: orders });
-    } else {
-      res.json({ success: false, message: "You are not admin" });
-    }
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false, message: "Error" });
-  }
-};
-
-// api for updating status
 const updateStatus = async (req, res) => {
   try {
-    let userData = await userModel.findById(req.body.userId);
-    if (userData && userData.role === "admin") {
-      await orderModel.findByIdAndUpdate(req.body.orderId, {
-        status: req.body.status,
-      });
-      res.json({ success: true, message: "Status Updated Successfully" });
-    }else{
-      res.json({ success: false, message: "You are not an admin" });
-    }
+    await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+    res.json({ success: true, message: "Status Updated" });
   } catch (error) {
-    console.log(error);
     res.json({ success: false, message: "Error" });
   }
 };
 
-export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    res.json({ success: false, message: "Not Verified" });
+  }
+};
+
+export { placeOrder, listOrders, userOrders, updateStatus, verifyOrder };
